d5ec19e503149e6885408e3cfb1d4c78dc0e23ad
Georgi Blazhev <georgi.lyubomirov.blazhev@gmail.com>
2022-01-07 12:53:50 +0200

    adding PasswordValidator

---
+++ src/WorkforceManagement.WEB/PasswordValidator.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WorkforceManagement.BLL.IServices;
+using WorkforceManagement.DAL.Entities;
+
+namespace WorkforceManagement.WEB
+{
+    public class PasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly IUserManager _userManager;
+
+        public PasswordValidator(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        //This method validates the user credentials and if successful teh IdentiryServer will build a token from the context.Result object
+        public async System.Threading.Tasks.Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            User user = await _userManager.FindByUserNameAsync(context.UserName);
+
+            if (user != null)
+            {
+                bool authResult = await _userManager.ValidateUserCredentials(context.UserName, context.Password);
+                if (authResult)
+                {
+                    List<string> roles = await _userManager.GetUserRolesAsync(user);
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+
+                    foreach (var role in roles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, role));
+                    }
+
+                    context.Result = new GrantValidationResult(subject: user.Id, authenticationMethod: "password", claims: claims);
+                }
+                else
+                {
+                    context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Invalid credentials");
+                }
+
+                return;
+            }
+            context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Invalid credentials");
+        }
+    }
+}
