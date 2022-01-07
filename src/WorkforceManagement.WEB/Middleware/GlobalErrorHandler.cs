48471cc32b2a44a5d28ecf7dbbb11c4913b6590c
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2022-01-07 16:41:57 +0200

    Folder strucutre in WEB and Global Error Handler

---
+++ src/WorkforceManagement.WEB/Middleware/GlobalErrorHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.WEB.Middleware
+{
+    public class GlobalErrorHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case KeyNotFoundException:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        // Unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    ExceptionType = $"{error.GetType().Name}",
+                    DefaultMessage = "There was an exception while trying to process your requests",
+                    SpecificMessage = $"{error.Message}"
+                });
+
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
