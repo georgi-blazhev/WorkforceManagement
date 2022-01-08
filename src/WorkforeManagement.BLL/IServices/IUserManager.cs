d71193181980cf9ef933662564a55c616488dd9c
Ivaylo Kalaydzhiev <ivailo.s.k@outlook.com>
2022-01-08 16:03:24 +0200

    Implement and celanup IUserManager

--- src/WorkforeManagement.BLL/IServices/IUserManager.cs
+++ src/WorkforeManagement.BLL/IServices/IUserManager.cs
@@ -11,16 +11,15 @@ namespace WorkforceManagement.BLL.IServices
 {
     public interface IUserManager
     {
-        Task CreateUserAsync(User user, string password);
-        Task<IdentityResult> DeleteUserAsync(User user);
-        Task<User> FindByUserNameAsync(string userName);
         Task<User> FindByIdAsync(string id);
-        Task<User> UpdateUserAsync(User user, string currentPassword, string newPassword);
-        Task<List<User>> GetAllAsync();
+        Task<User> FindByUserNameAsync(string userName);
         Task<IdentityResult> AddToRoleAsync(User user, string role);
-        Task<bool> ValidateUserCredentials(string userName, string password);
+        Task<List<User>> GetAllAsync();
         Task<List<string>> GetUserRolesAsync(User user);
-        Task<User> GetUserAsync(ClaimsPrincipal claimsPrincipal);
+        Task CreateUserAsync(User user, string password);
+        Task<User> UpdateUserAsync(User user, string currentPassword, string newPassword);
+        Task DeleteUserAsync(User user);
+        Task<bool> ValidateUserCredentials(string userName, string password);
         public void AssignUserToTeam(User user, Team team);
         public void UnassignUserToTeam(User user, Team team);
     }
