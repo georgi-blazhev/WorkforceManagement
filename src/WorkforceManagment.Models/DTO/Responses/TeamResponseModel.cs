916cfc93a8c6c5a7ebbfaf86cb302e2da6d928f6
Georgi Blazhev <georgi.lyubomirov.blazhev@gmail.com>
2022-01-10 16:04:27 +0200

    Adding TeamResponse

---
+++ src/WorkforceManagment.Models/DTO/Responses/TeamResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkforceManagement.Models.DTO.Responses
+{
+    public class TeamResponse
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TeamLeader { get; set; }
+    }
+}
